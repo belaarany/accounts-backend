10b294a2afe516ec9c82f8df34a070baf29abe49
Bela Arany <bela.arany@goabela.com>
2019-08-12 21:13:46 +0200

    updates

--- src/app/server.ts
+++ src/app/server.ts
@@ -4,44 +4,48 @@ import express from "express"
 import { App } from "~app/app"
 
 class Server {
-    private app: App
-    private server!: http.Server
+	private app: App
+	private server!: http.Server
 
-    constructor(app: App) {
-        this.app = app
+	constructor(app: App) {
+		this.app = app
 
-        winston.info("Server instantiated")
-    }   
+		winston.info("Server instantiated")
+	}
 
-    public listen(port: number | "@env"): Promise<void> {
-        return new Promise((resolve: () => void, reject: () => void) => {
-            if (port === "@env") {
-                port = Number(process.env.APP_PORT || "8020")
-            }
+	public listen(port: number | "@env"): Promise<void> {
+		return new Promise((resolve: () => void, reject: () => void) => {
+			if (port === "@env") {
+				port = Number(process.env.APP_PORT || "8020")
+			}
 
-            let express: express.Application = this.app.getExpress()
+			let express: express.Application = this.app.getExpress()
 
-            this.server = express.listen(port, () => {
-                // @ts-ignore
-                let at = this.server.address().address
-                winston.info(`Server started listening at '${at}${port}' in env '${express.settings.env}'`)
+			this.server = express.listen(port, () => {
+				// @ts-ignore
+				let at = this.server.address().address
+				winston.info(
+					`Server started listening at '${at}${port}' in env '${
+						express.settings.env
+					}'`
+				)
 
-                resolve()
-            })
-        })
-    }
+				resolve()
+			})
+		})
+	}
 
-    public shutdown(): Promise<void> {
-        return new Promise((resolve: () => void, reject: () => void) => {
-            let server: http.Server = this.server
-            
-            server.close(() => {
-                winston.info("Server did shutdown")
+	public shutdown(): Promise<void> {
+		return new Promise((resolve: () => void, reject: () => void) => {
+			let server: http.Server = this.server
 
-                resolve()
-            })
-        })
-    }
+			server.close(() => {
+				winston.info("Server did shutdown")
+
+				resolve()
+			})
+		})
+	}
 }
 
 export { Server }

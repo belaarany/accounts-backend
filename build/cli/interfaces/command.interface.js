6c657d1426f2bedc74b554d36b864cbe4406f451
Bela Arany <bela.arany@goabela.com>
2019-08-19 10:23:45 +0200

    Build

--- build/cli/interfaces/command.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ACommand = /** @class */ (function () {
-    function ACommand() {
-    }
-    return ACommand;
-}());
-exports.ACommand = ACommand;
-//# sourceMappingURL=command.interface.js.map

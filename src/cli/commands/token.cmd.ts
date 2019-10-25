a268fddc5a62da000be38a5b1d26337f8d59d342
Bela Arany <bela.arany@goabela.com>
2019-10-25 15:09:18 +0200

    Updates

--- src/cli/commands/token.cmd.ts
+++ src/cli/commands/token.cmd.ts
@@ -1,27 +1,29 @@
 import clear from "clear"
 import * as tokenHandler from "~app/utils/tokenHandler"
+import treeify from "treeify"
 import { Args, CommandInstance } from "vorpal"
 import { ACommand, ICommandLoad } from "../interfaces/command.interface"
 
 module.exports = class extends ACommand {
-    public app!: ICommandLoad["app"]
+	public app!: ICommandLoad["app"]
 
-    public load(props: ICommandLoad) {
-        this.app = props.app
-        
-        props.cli
-            .command("token:decode <token>")
-            .description("Decodes a token")
-            .action(this.decodeToken)
-    }
+	public load(props: ICommandLoad) {
+		this.app = props.app
 
-    public action
+		props.cli
+			.command("token:decode <token>")
+			.description("Decodes a token")
+			.action(this.decodeToken)
+	}
 
-    public decodeToken = (args: Args, done: () => void): void => {
-        const decoded = tokenHandler.decode(args.token)
+	public action
 
-        console.log({decoded})
+	public decodeToken = (args: Args, done: () => void): void => {
+		const decoded = tokenHandler.decode(args.token)
 
-        done()
-    }
+		console.log("token")
+		console.log(treeify.asTree(decoded, true))
+
+		done()
+	}
 }
